7716e3e6bbbfe1a31f81b1a249a622c0d2029303
Pieter <pieterj@live.nl>
2017-05-06 11:56:41 +0200

    no message

--- Cute Cargo/source/PlayState.hx
+++ Cute Cargo/source/PlayState.hx
@@ -1,40 +1,17 @@
 package;
 
 import flixel.FlxState;
-import flixel.text.FlxText;
-import flixel.FlxG;
-import flixel.input.touch.FlxTouch;
-import flixel.ui.FlxVirtualPad;
 
 class PlayState extends FlxState
 {
 	
-	var _text:FlxText;
-	var _int:Int;
-	var touch1:Float;
-	
 	override public function create():Void
 	{
-		_text = new FlxText(0, 0, 0, "0 - 0", 48);
-		add(_text);
-		
 		super.create();
 	}
 
 	override public function update(elapsed:Float):Void
 	{	
-		#if flash
-		_text.text = FlxG.mouse.screenX + " - " + FlxG.mouse.screenY + "";
-		#end
-		
-		#if mobile
-		for (touch in FlxG.touches.list){
-			if (touch.pressed)
-			{
-				_text.text = touch.screenX + " - " + touch.screenY + "";
-			}
-		}
-		#end
 		super.update(elapsed);
 	}
 }
